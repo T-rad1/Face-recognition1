2884ca837dccaf57bd9cb37240f9dd68d5008558
T-rad1 <t94576451@gmail.com>
2025-03-29 15:23:23 +0330

    Update httpp.py

--- httpp.py
+++ httpp.py
@@ -1,18 +1,31 @@
 import http.server
 import socketserver
-import os
+import socket
+import threading
+import time
+
+def get_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+def stop_server(httpd, duration):
+    time.sleep(duration)
+    print("\nServer stopped automatically after 10 minutes.")
+    httpd.shutdown()
+    print("stop reading")
 
 def httpp():
-    class CustomHandler(http.server.SimpleHTTPRequestHandler):
-        def do_GET(self):
-            if self.path == "CHECK.txt" :
-                self.path = "CHECK.txt"
-                return http.server.SimpleHTTPRequestHandler.do_GET(self)
-            else :
-                self.send_error(404, "File not found")
-
     PORT = 8000
+    Handler = http.server.SimpleHTTPRequestHandler
 
-    with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
-        print(f"Server started at http://192.168.1.55:{PORT}")
-        httpd.serve_forever()
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        ip = get_ip()
+        print(f"Server started at http://{ip}:{PORT}")
+
+        # اجرای تایمر برای متوقف کردن سرور بعد از  10 ثانیه
+        threading.Thread(target=stop_server, args=(httpd, 10), daemon=True).start()
+
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nServer stopped manually.")
+            httpd.server_close()
